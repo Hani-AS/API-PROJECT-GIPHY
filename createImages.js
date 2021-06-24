717fc812ec747799deb8fda02c4d8192136387fb
Hani al Sabti <h.sabti87@gmail.com>
2021-06-24 13:21:16 +0200

    Injected images into trending/search section and made the search function work.

--- createImages.js
+++ createImages.js
@@ -1,8 +1,27 @@
 "use strict";
-import main from "./main.js";
+import { main } from "./main.js";
 
-const createImages = (trendingData) => {
-  console.log(trendingData);
+const createImages = (data) => {
+  const trendingContainer = document.createElement("div");
+  trendingContainer.className = "container container_body";
+  const row = document.createElement("div");
+  row.className = "row row-cols-2 row-cols-md-4 g-2";
+  row.setAttribute("data-masonry", '{"percentPosition": true }');
+  const polluteImages = data.forEach((element) => {
+    const col = document.createElement("div");
+    col.className = "col";
+    const card = document.createElement("div");
+    card.className = "card border-3 rounded border-light";
+    const img = document.createElement("img");
+    img.className = "img-fluid w-100";
+    img.src = element.images.downsized.url;
+    row.appendChild(col);
+    col.appendChild(card);
+    card.appendChild(img);
+  });
+  const containerHeader = document.querySelector(".container_header");
+  containerHeader.appendChild(trendingContainer);
+  trendingContainer.appendChild(row);
 };
 
 export default createImages;
---
+++ searchBtnHandler.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import createImages from "./createImages.js";
+import { requestData, API_KEY } from "./main.js";
+
+const searchBtnHandler = () => {
+  const btn = document.querySelector(".btn");
+  btn.addEventListener("click", async () => {
+    const formInput = document.querySelector(".form-control").value.trim();
+    const searchData = await requestData(
+      `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${formInput}&limit=12&offset=0&rating=g&lang=en`
+    );
+    const clearContent = document.querySelector(".container_body");
+    clearContent.parentElement.removeChild(clearContent);
+    createImages(searchData.data);
+  });
+};
+
+export default searchBtnHandler;
